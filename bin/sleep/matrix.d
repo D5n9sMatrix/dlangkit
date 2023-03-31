e7f56ed0ec7c43a8ac4445abe78207d04ebdf3db
D5n9sMatrix <denis.02live@hotmail.com>
2023-03-31 01:35:04 -0300

    sleep matrix proof die in live to life

---
+++ bin/sleep/matrix.d
@@ -0,0 +1,46 @@
+module sleep.matrix;
+
+interface matrix {
+/*
+The existence of inverses for elementary row operations and hence for elementary operations on a system
+of equations, gives:
+Proof of Theorem 1.1.1. Suppose that a system of linear equations is transformed into a new system
+by a sequence of elementary operations. Then every solution of the original system is automatically a
+solution of the new system because adding equations, or multiplying an equation by a nonzero number,
+always results in a valid equation. In the same way, each solution of the new system must be a solution
+to the original system because the original system can be obtained from the new one by another series of
+elementary operations (the inverses of the originals). It follows that the original and new systems have the
+same solutions. This proves Theorem 1.1.1.
+*/
+class proof {
+
+        void die(int x, y)(ref str)
+        {
+            str readLink(R)(R link)
+            if (str!R || link!R);
+            str writeLink(R)(R link)
+            if (str!R || link!R);
+
+        }
+
+        void live(int x, y)(ref str)
+        {
+            str readLink(R)(R link)
+            if (str!R || link!R);
+            str writeLink(R)(R link)
+            if (str!R || link!R);
+
+        }
+
+        void life(int x, y)(ref str)
+        {
+            str readLink(R)(R link)
+            if (str!R || link!R);
+            str writeLink(R)(R link)
+            if (str!R || link!R);
+
+        }
+
+    }
+
+}
